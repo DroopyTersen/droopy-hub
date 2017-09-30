43d7b48235489a9f2d72ff5019f0c458e2b21166
Andrew Petersen <andrew.petersen15@gmail.com>
2017-09-30 13:14:55 -0500

    fixed object spread issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Freezer = require("freezer-js");
-
+var objectAssign = require('object-assign');
 const defaultOptions = {
     cacheKey: "droopy-hub-cache",
     cacheEnabled: true
@@ -8,8 +8,7 @@ const defaultOptions = {
 var createHub = function(defaultState, opts) {
     opts = Object.assign({}, defaultOptions, opts);
     var cache = setupCache(opts.cacheKey);
-    
-    var initialState = { ...defaultState, ...(opts.cacheEnabled ? cache.get() : {}) };
+    var initialState = objectAssign({}, defaultState, (opts.cacheEnabled ? cache.get() : {}));
     var state = new Freezer(initialState);
 
     var hub = state.getEventHub();
@@ -46,3 +45,33 @@ var setupCache = function(key = "droopy-hub-cache") {
 }
 
 module.exports = { createHub };
+
+if (typeof Object.assign != 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+      value: function assign(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+  
+        var to = Object(target);
+  
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+  
+          if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      },
+      writable: true,
+      configurable: true
+    });
+  }
