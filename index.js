0f65f327d812b03647ac654b1a407264d3f2d483
Andrew Petersen <andrew.petersen15@gmail.com>
2017-09-29 22:45:35 -0500

    got initial version working

---
+++ index.js
@@ -0,0 +1,48 @@
+var Freezer = require("freezer-js");
+
+const defaultOptions = {
+    cacheKey: "droopy-hub-cache",
+    cacheEnabled: true
+}
+
+var createHub = function(defaultState, opts) {
+    opts = Object.assign({}, defaultOptions, opts);
+    var cache = setupCache(opts.cacheKey);
+    
+    var initialState = { ...defaultState, ...(opts.cacheEnabled ? cache.get() : {}) };
+    var state = new Freezer(initialState);
+
+    var hub = state.getEventHub();
+    Object.defineProperty(hub, "state", {
+        get: () => state.get()
+    })
+    hub.cache = cache;
+    hub.cacheState = () => cache.set(state.get().toJS());
+    hub.trigger = (key, ...args) => hub.emit(key, ...args);
+
+    
+    return hub;
+}
+
+var setupCache = function(key = "droopy-hub-cache") {
+    return {
+        KEY: key,
+        get() {
+            try {
+                var cachedVal = localStorage.getItem(key);
+                return cachedVal ? JSON.parse(cachedVal) : null;
+    
+            } catch (ex) {
+                return null;
+            }
+        },
+        set(val) {
+            return localStorage.setItem(key, JSON.stringify(val))
+        },
+        clear() {
+            localStorage.removeItem(key);
+        }
+    }
+}
+
+module.exports = { createHub };
